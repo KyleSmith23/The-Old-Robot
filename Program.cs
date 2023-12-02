d5e6d0b15943deda5da715b7c76f3ca7a8cc94ea
kyle- <kyle-smith1@outlook.com>
2023-12-02 15:45:20 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+
+// Classes
+
+using System.Security.Cryptography.X509Certificates;
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public RobotCommand?[] Commands { get; } = new RobotCommand?[3];
+    public void Run()
+    {
+        foreach (RobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public abstract class RobotCommand
+{
+     public abstract void Run(Robot robot);      
+}
+
+public class OnCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
